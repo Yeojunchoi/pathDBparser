d16d13308e05e120098a0de354445aa16597bafa
airon <yeojun0702@gmail.com>
2021-02-10 12:11:21 +0900

    route class working

---
+++ route.h
@@ -0,0 +1,60 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include "node.h"
+
+struct Waypoint{
+    double  Lat;
+    double  Lon;
+    float   Alt;
+    float   param1;
+    float   param2;
+    float   param3;
+    float   param4;
+    int     command;
+    int     doJumpId;
+    int     AltMode;
+    bool    autocontinue;
+    int     frame;
+};
+
+
+class Route{
+    public:
+
+
+
+
+
+
+    private:
+        Node node1;
+        Node node2;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
